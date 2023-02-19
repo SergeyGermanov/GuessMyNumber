6a0d181e46923b3c00159420fa89414517dffba7
Sergey Germanov <husky@Sergeys-MacBook-Pro.local>
2023-02-19 17:22:37 +1300

    startgame screen styling

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,30 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StarGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StarGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    // elevation is Android specific shadow styling
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+    backgroundColor: "#72063c",
+  },
+});
