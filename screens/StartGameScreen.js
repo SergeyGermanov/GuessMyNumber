57359d1bfdfa3a8fbeb1e38482f5b5bfe4ba3b66
Sergey Germanov <husky@Sergeys-MacBook-Pro.local>
2023-03-25 17:35:29 +1300

    add gloabl colors

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import { TextInput, View, StyleSheet, Alert } from "react-native";
 import { useState } from "react";
 import PrimaryButton from "../components/PrimaryButton";
+import Colors from "../constants/colors";
 
 function StarGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
@@ -68,15 +69,15 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
-    backgroundColor: "#3b021f",
+    backgroundColor: Colors.primary800,
   },
   numberInput: {
     height: 50,
     width: 50,
     fontSize: 32,
-    borderBottomColor: "#ddb52f",
+    borderBottomColor: Colors.accent500,
     borderBottomWidth: 2,
-    color: "#ddb52f",
+    color: Colors.accent500,
     marginVertical: 8,
     fontWeight: "bold",
     textAlign: "center",
